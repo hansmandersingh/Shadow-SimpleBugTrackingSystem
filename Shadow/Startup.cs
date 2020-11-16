011492022ba7db5294ae254f09d1897a7a0d808a
Hansmander <hansmandersingh1998@gmail.com>
2020-11-15 22:02:14 -0600

    Add project files.

---
+++ Shadow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shadow.Startup))]
+namespace Shadow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

4f7628a3f5a8a340c8b92e53368a3542856f85b4
Hansmander <hansmandersingh1998@gmail.com>
2020-11-15 22:27:42 -0600

    Added ProjectUser class to break many to many relation b/w projects and users

--- Shadow/Models/Project.cs
+++ Shadow/Models/Project.cs
@@ -9,5 +9,10 @@ namespace Shadow.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<ProjectUser> ProjectUsers { get; set; }
+        public Project()
+        {
+            ProjectUsers = new HashSet<ProjectUser>();
+        }
     }
 }
---
+++ Shadow/Models/ProjectUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shadow.Models
+{
+    public class ProjectUser
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}

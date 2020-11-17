514f790a23ebf18a3d51eb3c8ea705ef4d75e5fc
Hansmander <hansmandersingh1998@gmail.com>
2020-11-17 11:39:10 -0600

    Extending on the admin business layer in creating project

--- Shadow/BL/AdminBusinessLayer.cs
+++ Shadow/BL/AdminBusinessLayer.cs
@@ -10,6 +10,7 @@ namespace Shadow.BL
     public class AdminBusinessLayer
     {
         UserAndRolesRepository UserAndRolesRepository = new UserAndRolesRepository();
+        ProjectRepository ProjectRepository = new ProjectRepository();
         public bool AssignUserToRole(string adminId, string userId, string roleName)
         {
             if (UserAndRolesRepository.CheckIfUserIsInRole(adminId, "admin"))
@@ -53,6 +54,28 @@ namespace Shadow.BL
         public List<ApplicationUser> GetAllUsers()
         {
             return UserAndRolesRepository.GetAllUsers();
+        }
+
+        public bool AddANewProject(string adminId, string projectName)
+        {
+            if (UserAndRolesRepository.CheckIfUserIsInRole(adminId, "admin"))
+            {
+                var result = ProjectRepository.CreateProject(projectName);
+                if (result)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        public void EditProject(string adminId,Project project)
+        {
+            if (UserAndRolesRepository.CheckIfUserIsInRole(adminId, "admin"))
+            {
+
+            }
         }
     }
 }

cd2beca0e4f2b4d00d2d369305f2edbaf3cf2ca5
Hansmander <hansmandersingh1998@gmail.com>
2020-11-19 12:18:55 -0600

    Add comment works on the admin tickets

--- Shadow/Controllers/AdminController.cs
+++ Shadow/Controllers/AdminController.cs
@@ -176,5 +176,21 @@ namespace Shadow.Controllers
 
             return View(AllTickets.ToPagedList(pageNumber, pageSize));
         }
+
+        public ActionResult AddComment(int ticketId)
+        {
+            ViewBag.ticketId = ticketId;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddComment(int ticketId, string commentText)
+        {
+            var result = AdminBusinessLayer.AddCommentOnTicket(User.Identity.GetUserId(), ticketId, commentText);
+
+            if (result)
+                return RedirectToAction("GetAllTickets");
+            else
+                return View(ticketId);
+        }
     }
 }
